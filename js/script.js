7cfd24d667c7b3f2d7442fa9d8334b8e74ffe3b7
Luciano Bitencourt <lucbite@gmail.com>
2022-12-20 18:15:01 -0300

    update
    update

--- js/script.js
+++ js/script.js
@@ -1,9 +1,82 @@
 
 // variavel
 
-console.log();
+// nome = prompt("digite seu nome");
+
+// idade = Number(prompt("Digite sua idade"));
+
+// Aplcando templeteString
+// console.log(`O Srº ${nome} tem ${idade}`);
+
+// Aplicando concatenação (+)
+
+// console.log('O Srº ' + nome + " tem " + idade);
+
+
+// var nome = 'luciano';
+
+/**$peso1 = Number(prompt("Informe seu primeiro peso"));
+
+$peso2 = Number(prompt("Informe sua segundo peso"));
+
+total = ($peso1 + $peso2);
+
+console.log(total.toFixed(2).replace('.',","));
+*/
+
+// console.log(nome.toUpperCase());
 
 
 
+// auto-atribuição
+/**var n=3;
+
+n+=4;
+n-=2;
+n*=5;
+n/=2;
+n%=3;
+n**=12
+
+console.log(n);
+// incremento
+
+var x=10;
+
+++x;
 
 
+console.log(x);*/
+
+// Operadores Relacionais [ > , >= , < , <= , != , == , ===]
+/**var n1 = '8' ;
+var n2 = 8;
+
+console.log(n1 > n2);
+console.log(n1 >= n2);
+console.log(n1 < n2);
+console.log(n1 <= n2);
+console.log(n1 != n2);
+
+console.log(n1 == n2);
+console.log(n1 === n2);
+
+console.log(typeof n1);
+console.log(typeof n2);*/
+
+// OPERADOR  LOGICO AND - OR  -NOT 
+
+var $logico = false && false
+
+console.log($logico);
+
+var $logico2 = false || true
+
+console.log($logico2);
+
+
+// OPERADOR TERNARIO (teste logico)? TRUE : FALSE
+
+var a = 12
+var b = 10
+console.log ((a >= b ) ? true : false);  
