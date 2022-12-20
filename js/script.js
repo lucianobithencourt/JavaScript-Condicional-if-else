e0099f28678088d6499db66d91ef356a7194cd94
Luciano Bitencourt <lucbite@gmail.com>
2022-12-20 09:58:28 -0300

    update
    update

---
+++ js/script.js
@@ -0,0 +1,58 @@
+
+// alert("Primeiro Script")
+
+/* ----Comentário de Varias linhas ----
+ 
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam, quod voluptas. Fugit delectus, rem totam voluptatum facilis exercitationem recusandae optio sunt ex cum quisquam similique alias necessitatibus reiciendis tempora quas? 
+ */
+
+// Comentario de uma unica linha
+
+// Declarando Variaveis
+
+// string 
+// var nome = prompt("Digite seu nome",'seu nome');
+
+// Number
+// var idade = Number(prompt("Sua idade","Digite aqui"));
+
+// var altura = Number(prompt("Sua idade","Digite aqui"));
+
+// boolean
+// var texte_1 = true;
+// var texte_2 = false;
+
+/** Exibir Script no cosole e tela 
+console.log(typeof idade);
+
+
+// alert("O sr." + nome + " tem "+idade+ " de idade " + " e " + altura + " de altura")
+
+console.log("O sr." + nome + " tem "+idade+ " de idade " + " e " + altura + " de altura");
+
+document.write("<h3>O sr° " + nome + " tem "+idade+ " de idade " + " e " + altura + " de altura.</h3> ")*/ 
+
+// TROCAR VALORES DE UMA VARIAVEL
+
+var A = 10;
+var B = 20;
+var C = null;
+
+console.log(A);
+console.log(B);
+console.log(C);
+// ---------------------------
+
+var C = A;
+var A = B;
+var B = C;
+
+
+console.log(A);
+console.log(B);
+console.log(C);
+
+
+
+
+
